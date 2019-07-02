b9fa7067d30ce53fb70494cc8341eacf9e1956da
Majo-Cortez <52359718+aplicativ@users.noreply.github.com>
2019-07-01 19:39:46 -0500

    Programa modificado

--- Ejercicio.java
+++ Ejercicio.java
@@ -2,10 +2,16 @@
  public class Ejercicio{
   public static void main(String[] args){
   float num,den;
+  float num0,den0;
   Scanner lee=new Scanner(System.in);
   System.out.println("Ingrese el numerador");
   num=lee.nextFloat();
   System.out.println("Ingrese el denominador");
+  den=lee.nextFloat();
+  Scanner lee=new Scanner(System.in);
+  System.out.println("Ingrese el numerador0");
+  num=lee.nextFloat();
+  System.out.println("Ingrese el denominador0");
   den=lee.nextFloat();
    
 }
